585782e2ac2ef7c1949a743b8d9c276b28f463b9
Piyush Kumar <pk6686742@gmail.com>
2026-04-21 15:53:30 +0000

    Completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { User } from '../models/user.model.js';
+import { User } from "../models/user.model.js";
 
 /**
  * TODO: List all users (Admin only)
@@ -9,6 +9,10 @@ import { User } from '../models/user.model.js';
 export async function listUsers(req, res, next) {
   try {
     // Your code here
+    const users = await User.find().select("-password");
+    return res.status(200).json({
+      users,
+    });
   } catch (error) {
     next(error);
   }
@@ -25,6 +29,17 @@ export async function listUsers(req, res, next) {
 export async function getUser(req, res, next) {
   try {
     // Your code here
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      return res.status(404).json({
+        error: {
+          message: "User not found",
+        },
+      });
+    }
+    return res.status(200).json({
+      user,
+    });
   } catch (error) {
     next(error);
   }
@@ -41,6 +56,17 @@ export async function getUser(req, res, next) {
 export async function deleteUser(req, res, next) {
   try {
     // Your code here
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        error: {
+          message: "User not found",
+        },
+      });
+    }
+    return res.status(200).json({
+      message: "User deleted successfully",
+    });
   } catch (error) {
     next(error);
   }
